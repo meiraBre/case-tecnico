4b6c1756333b263a5bcbff1804d314f09ec19f4d
Brenda <bremeirah@gmail.com>
2025-08-25 14:06:06 -0300

    Dados dos arquivos CSV inseridos no banco de dados

--- case_api/data.py
+++ case_api/data.py
@@ -1,5 +1,16 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
-# Carregamento único dos CSVs
-users_df = pd.read_csv("users.csv")
-metrics_df = pd.read_csv("metrics.csv")
+def load_data():
+    DATABASE_URL = "sqlite:///database/banco.db"
+    engine = create_engine(DATABASE_URL)
+
+    # Inserir métricas
+    df_metrics = pd.read_csv("metrics.csv")
+    df_metrics.to_sql("metricas", engine, if_exists="append", index=False)
+
+if __name__ == "__main__":
+    load_data()
+    print("✅ Dados inseridos com sucesso!")
+
+
